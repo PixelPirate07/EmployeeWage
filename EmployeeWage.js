9e226d657a3e29003b390a75852e8f3c02626e0c
PixelPirate07 <pandeydhairya02@gmail.com>
2025-03-20 16:31:36 +0530

    UC 9: Perform Operations Using Arrow Functions

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -162,6 +162,7 @@ console.log("UC 7G - Number of Days Emp Worked: " +
             totalWorkingDays = 0;
             empDailyWageArr = new Array();
             empDailyWageMap = new Map();
+            let empDailyHrsMap = new Map();
             
             function calcDailyWage(empHrs) {
                 return empHrs * WAGE_PER_HOUR;
@@ -175,6 +176,8 @@ console.log("UC 7G - Number of Days Emp Worked: " +
                 totalEmpHrs += empHrs;
                 empDailyWageArr.push(calcDailyWage(empHrs));
                 empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+                empDailyHrsMap.set(totalWorkingDays, empHrs);
+                
             }
             
             console.log(empDailyWageMap);
@@ -185,4 +188,30 @@ console.log("UC 7G - Number of Days Emp Worked: " +
             
             console.log("UC8 - Emp Wage Map totalHrs: " +
                 Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
-            
+
+// UC 9 Arrow Functions
+
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0)
+                                 .reduce(findTotal, 0);
+console.log("UC9A – Emp Wage with Arrow.: " + " Total Hours: " +
+            totalHours + " Total Wages: " + totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+
+empDailyHrsMap.forEach((value, key, map) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+
+console.log("Full Working Days: " + fullWorkingDays);
+console.log("Part Working Days: " + partWorkingDays);
+console.log("Non Working Days: " + nonWorkingDays);
+                
